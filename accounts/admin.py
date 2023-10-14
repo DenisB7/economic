e868705d5e522b4723a022ae0aa4ef3ce2ff8fb5
DenisB7 <denzelbe1987@gmail.com>
2023-10-14 20:20:16 +0500

    Create custom user model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from accounts.models import UserCustom
+
+
+@admin.register(UserCustom)
+class UserCustomAdmin(admin.ModelAdmin):
+    list_display = (
+        "username",
+        "email",
+        "first_name",
+        "last_name",
+        "is_staff",
+        "is_superuser",
+        "last_login",
+        "date_joined",
+    )
+    list_filter = (
+        "username",
+        "email",
+        "first_name",
+        "last_name",
+        "is_staff",
+        "is_superuser",
+        "last_login",
+        "date_joined",
+    )
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class UserCustom(AbstractUser):
+    """We can remove and add desired additional fields like this:
+
+    new_field = models.CharField(max_length=300, null=True)
+    ---
+    NOTE:
+    When you will have new model which you will need to link with User model,
+    please don't use standard User model for ForeignKey, since we modified it, take it from settings.py:
+
+    from trial_assignment.settings import AUTH_USER_MODEL
+
+    class AnotherModel(models.Model):
+        user = models.ForeignKey(AUTH_USER_MODEL, ...)
+    ---
+    """
+
+    GENDERS = (
+        ("male", "Male"),
+        ("female", "Female"),
+        ("other", "Other"),
+    )
+
+    gender = models.CharField(max_length=30, choices=GENDERS)
+    age = models.PositiveIntegerField(null=True)
+
+    def __str__(self):
+        return f"USERNAME: {self.username} EMAIL: {self.email}"
