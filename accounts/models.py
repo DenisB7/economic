666c678760f6824dcf8ef587381e9ffd9de54599
DenisB7 <denzelbe1987@gmail.com>
2023-10-14 20:27:11 +0500

    Fix docstring for custom user model

--- accounts/models.py
+++ accounts/models.py
@@ -3,19 +3,17 @@ from django.contrib.auth.models import AbstractUser
 
 
 class UserCustom(AbstractUser):
-    """We can remove and add desired additional fields like this:
+    """This is custom user model, please pay attention to the instructions below
 
-    new_field = models.CharField(max_length=300, null=True)
-    ---
-    NOTE:
     When you will have new model which you will need to link with User model,
-    please don't use standard User model for ForeignKey, since we modified it, take it from settings.py:
+    please don't use standard User model for ForeignKey, since we modified it, take it from settings.py.
+
+    Check example below:
 
     from trial_assignment.settings import AUTH_USER_MODEL
 
     class AnotherModel(models.Model):
         user = models.ForeignKey(AUTH_USER_MODEL, ...)
-    ---
     """
 
     GENDERS = (
