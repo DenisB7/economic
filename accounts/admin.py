daf9551a70f2948eec6eb8a482a67cf24b35a5b0
DenisB7 <denzelbe1987@gmail.com>
2023-10-15 13:05:09 +0500

    Add countries and cities models

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
 
-from accounts.models import UserCustom
+from accounts.models import City, Country, UserCustom
+
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    list_display = ("name", "country")
+    list_filter = ("country__name",)
+
+
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    list_filter = ("name",)
 
 
 @admin.register(UserCustom)
--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from cities_light.models import Country, City
+
+
+class Country(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class City(models.Model):
+    name = models.CharField(max_length=300)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE, related_name="cities")
+
+    def __str__(self):
+        return self.name
 
 
 class UserCustom(AbstractUser):
@@ -25,8 +39,8 @@ class UserCustom(AbstractUser):
 
     gender = models.CharField(max_length=30, choices=GENDERS)
     age = models.PositiveIntegerField(null=True)
-    country = models.ForeignKey(Country, on_delete=models.SET_NULL, null=True, related_name="users_custom")
-    city = models.ForeignKey(City, on_delete=models.SET_NULL, null=True, related_name="users_custom")
+    country = models.ForeignKey(Country, on_delete=models.SET_NULL, related_name="users_custom", null=True)
+    city = models.ForeignKey(City, on_delete=models.SET_NULL, related_name="users_custom", null=True)
 
     def __str__(self):
         return f"USERNAME: {self.username} EMAIL: {self.email}"
