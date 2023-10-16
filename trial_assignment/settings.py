178ce5cfec00b75a0a531ab36c295b52a17033d6
DenisB7 <denzelbe1987@gmail.com>
2023-10-16 21:30:25 +0500

    Fix static

--- trial_assignment/settings.py
+++ trial_assignment/settings.py
@@ -141,9 +141,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_new')
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    os.path.join(BASE_DIR, 'static'),
+    'static'
 ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
