4f9b086c68c14ee38d9ca23b23e69ca421c28401
DenisB7 <denzelbe1987@gmail.com>
2023-10-16 19:32:17 +0500

    Add names to urls to fix tests of sales app

--- sales/urls.py
+++ sales/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from sales import views
 
 urlpatterns = [
-    path("api/v1/sale_statistics/", views.SaleStatistics.as_view()),
-    path("api/v1/sales/$<int:pk>/", views.SaleDetail.as_view()),
-    path("api/v1/sales/", views.SaleList.as_view()),
+    path("api/v1/sale_statistics/", views.SaleStatistics.as_view(), name="sale-statistics"),
+    path("api/v1/sales/$<int:pk>/", views.SaleDetail.as_view(), name="sale-detail"),
+    path("api/v1/sales/", views.SaleList.as_view(), name="sales-list"),
 ]
