c0ddd555a6ad4a3b2447f08c00852d6fc79caae8
DenisB7 <denzelbe1987@gmail.com>
2023-10-16 10:34:26 +0500

    Add user management endpoints

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,12 +1,13 @@
-# from django.urls import path
-# from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
 
-# from accounts.views import MainView, SignupView
+from accounts.views import UserRetrieveUpdateView, CountryListView, LoginView, LogoutView
 
-# app_name = 'accounts'
-# urlpatterns = [
-#     path('', MainView.as_view(), name="main"),
-#     path('accounts/register', SignupView.as_view(), name="signup"),
-#     path("accounts/login", LoginView.as_view(template_name="app_accounts/login.html"), name="login"),
-#     path("accounts/logout", LogoutView.as_view(), name="logout"),
-# ]
+
+urlpatterns = [
+    path('api/v1/login/', LoginView.as_view(), name='login'),
+    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/v1/logout/', LogoutView.as_view(), name='logout'),
+    path('api/v1/users/$<int:pk>/', UserRetrieveUpdateView.as_view(), name='user_detail'),
+    path('api/v1/countries/', CountryListView.as_view(), name='country_list'),
+]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,58 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+from rest_framework import generics, status
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework_simplejwt.exceptions import TokenError
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+from accounts.models import Country, UserCustom
+from accounts.serializers import (
+    CountrySerializer,
+    CustomTokenObtainPairSerializer,
+    UserSerializer,
+)
+
+
+User = get_user_model()
+
+
+class UserRetrieveUpdateView(generics.RetrieveUpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated, IsAdminUser]
+    authentication_classes = [JWTAuthentication]
+
+
+class CountryListView(generics.ListAPIView):
+    queryset = Country.objects.all()
+    serializer_class = CountrySerializer
+    permission_classes = [IsAuthenticated, IsAdminUser]
+    authentication_classes = [JWTAuthentication]
+
+
+class LoginView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
+
+
+class LogoutView(generics.GenericAPIView):
+    permission_classes = [IsAuthenticated, IsAdminUser]
+    authentication_classes = [JWTAuthentication]
+
+    def get(self, request):
+        """This method is used to logout user."""
+        
+        # Blacklist the refresh token
+        refresh_token = request.COOKIES.get("refresh")
+        if refresh_token:
+            try:
+                RefreshToken(refresh_token).blacklist()
+            except TokenError:
+                return Response(
+                    {"detail": "Invalid token"},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+
+        return Response(status=status.HTTP_200_OK)
